83dc47316abc3ef36fd3a3e09babbad752817ccd
mikelovex <maicon.rafael17@gmail.com>
2021-02-28 01:21:11 -0300

    ajustes nos projetos

--- src/components/Projetos/index.js
+++ src/components/Projetos/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 import {Container, Cards, Card} from './styles'
 
-import ecoletaImg from '../../assets/ecoleta.png'
+import hotelImg from '../../assets/hotel.png'
 import beatImg from '../../assets/beat.png'
 import pokemonImg from '../../assets/pokemon.png'
-import twitterImg from '../../assets/twitter.png'
+import gitfinderImg from '../../assets/ginfinder.png'
 
 function Projetos() {
 
@@ -14,10 +14,10 @@ function Projetos() {
        <Container id="projetos">
            <h1>Projetos</h1>
            <Cards>
-               <Card href="https://github.com/Mikelovex/Ecoleta" target="blank">
-                    <h2>Ecoleta</h2>
-                    <img src={ecoletaImg} alt="ecoleta"/>
-                    <p>Um marketplace de coleta de resíduos.</p>
+               <Card href="https://layout-hotel.vercel.app/" target="blank">
+                    <h2>Layout Hotel</h2>
+                    <img src={hotelImg} alt="ecoleta"/>
+                    <p>Um site para um hotel fictício</p>
                </Card>
                <Card href="https://beat-maker.vercel.app/" target="blank">
                     <h2>Beat Maker</h2>
@@ -29,10 +29,10 @@ function Projetos() {
                     <img src={pokemonImg} alt="pokedex"/>
                     <p>Um CRUD de pokemon, onde é possivel adicionar novos pokemons.</p>
                </Card>
-               <Card href="https://github.com/Mikelovex/Twitter-interface-clone" target="blank">
-                    <h2>Twitter Interface</h2>
-                    <img src={twitterImg} alt="twitter-interface"/>
-                    <p>Clone da interface do Twitter</p>
+               <Card href="https://gitfinder-wine.vercel.app/" target="blank">
+                    <h2>Github Finder</h2>
+                    <img src={gitfinderImg} alt="twitter-interface"/>
+                    <p>uma aplicação para encontrar usuários do github</p>
                </Card>
            </Cards>
        </Container>
